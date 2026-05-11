645aa349e49d64b9b8a47978c9c7f5178044223b
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-11 20:09:52 -0300

    feat(auth): implement jwt validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,15 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [UserModule, JwtModule.register({
     secret:process.env.JWT_SECRET,
-    signOptions: { expiresIn: '6h'},
+    signOptions: { expiresIn: '6h'}
 
   })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+//guard utilizado para proteger a autenticação de login
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   canActivate(context: ExecutionContext) {
