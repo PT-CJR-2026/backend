214e3068e980b533151b0f4bbceccb945a48ca88
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-14 20:36:34 -0300

    fix(jwtstrategy): Não estava lendo o secret, tive que fazer adaptações

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,12 +7,35 @@ import { UsuarioPayload } from '../models/UsuarioPayload';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new Error('JWT_SECRET não definido');
+    }
+
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+
+      secretOrKey: secret,
     });
   }
+
+
+
+//estava tendo problema com essa estrutura. Não estava lendo o secret
+// @Injectable()
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//   constructor() {
+//     super({
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       ignoreExpiration: false,
+//       secretOrKey: process.env.JWT_SECRET,
+//     });
+//   }
 
   async validate(payload: UsuarioPayload): Promise<UserFromJwt> {
     return {
