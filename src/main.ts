ea138b9bc5a3cc0721db732f77429e9f5b1709ec
Romm-0 <rafamelatti@gmail.com>
2026-05-16 18:00:47 -0300

    fix: comunicação com o front

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			//forbidNonWhitelisted: true,
-			transform: true,
-		}),
-	);
+  // Permite o front (3000) acessar apenas esse back (3001)
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
-	await app.listen(3001);
+  // Arruma os dados para poderem ser lidos pelo back
+  app.use(require('express').json());
+  app.use(require('express').urlencoded({ extended: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(3001);
 }
-
 bootstrap();
