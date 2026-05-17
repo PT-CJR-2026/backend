ae545ad1ba01a41a442ec17b1a807b9c0bc28fa1
Romm-0 <rafamelatti@gmail.com>
2026-05-16 21:36:05 -0300

    style: produtoId trocado por produto_id

--- src/imagem-produto/imagem-produto.controller.ts
+++ src/imagem-produto/imagem-produto.controller.ts
@@ -6,44 +6,44 @@ import { ImagemProdutoService } from './imagem-produto.service';
 import { CreateImagemProdutoDto } from './dto/create-imagem-produto.dto';
 import { UpdateImagemProdutoDto } from './dto/update-imagem-produto.dto';
 
-@Controller('produtos/:produtoId/imagens')
+@Controller('produtos/:produto_id/imagens')
 export class ImagemProdutoController {
     constructor(private readonly imagemProdutoService: ImagemProdutoService) {}
 
 
     @Post()
     create(
-        @Param('produtoId', ParseIntPipe) produtoId: number,
+        @Param('produto_id', ParseIntPipe) produto_id: number,
         @Body() dto: CreateImagemProdutoDto,
     ) {
-        return this.imagemProdutoService.create(produtoId, dto);
+        return this.imagemProdutoService.create(produto_id, dto);
     }
 
     @Get()
-    findAll(@Param('produtoId', ParseIntPipe) produtoId: number) {
-        return this.imagemProdutoService.findAll(produtoId);
+    findAll(@Param('produto_id', ParseIntPipe) produto_id: number) {
+        return this.imagemProdutoService.findAll(produto_id);
     }
 
     @Get(':id')
-    findOne(@Param('produtoId', ParseIntPipe) produtoId: number) {
-        return this.imagemProdutoService.findAll(produtoId);
+    findOne(@Param('produto_id', ParseIntPipe) produto_id: number) {
+        return this.imagemProdutoService.findAll(produto_id);
     }
 
     @Put(':id')
     update(
-        @Param('produtoId', ParseIntPipe) produtoId: number,
+        @Param('produto_id', ParseIntPipe) produto_id: number,
         @Param('id', ParseIntPipe) id: number,
         @Body() dto: UpdateImagemProdutoDto,
     ) {
-        return this.imagemProdutoService.update(produtoId, id, dto);
+        return this.imagemProdutoService.update(produto_id, id, dto);
     }
 
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
     remove(
-        @Param('produtoId', ParseIntPipe) produtoId: number,
+        @Param('produto_id', ParseIntPipe) produto_id: number,
         @Param('id', ParseIntPipe) id: number,
     ) {
-        return this.imagemProdutoService.remove(produtoId, id);
+        return this.imagemProdutoService.remove(produto_id, id);
     }
 }
