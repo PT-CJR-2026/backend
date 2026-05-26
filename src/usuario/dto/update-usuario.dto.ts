fe7ff91bd7e087cae357e4553d4fc81a3727d0e0
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-26 15:13:03 -0300

    feat(Usuario): Criação de novas rotas de edião de rotas.

--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsEmail, IsString, Matches, MaxLength, MinLength, IsOptional } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateSenhaDto {
+  @IsString()
+  senha_antiga!: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'senha muito fraca',
+  })
+  nova_senha!: string;
+}
+
+export class UpdateEmailDto {
+  @IsEmail()
+  email!: string;
+}
+
+export class UpdateUsernameDto {
+  @IsString()
+  username!: string;
+}
+
+export class UpdateNomeDto {
+  @IsString()
+  nome!: string;
+}
