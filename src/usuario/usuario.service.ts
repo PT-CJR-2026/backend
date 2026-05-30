c212a64b16d47dcd529e5034832fd80c9f6ec6d8
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-29 23:11:04 -0300

    fix:Corrigindo a remoção do getMe

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,6 +1,16 @@
-import { Injectable, BadRequestException, ConflictException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateSenhaDto, UpdateEmailDto, UpdateUsernameDto, UpdateNomeDto } from './dto/update-usuario.dto';
+import {
+  UpdateSenhaDto,
+  UpdateEmailDto,
+  UpdateUsernameDto,
+  UpdateNomeDto,
+} from './dto/update-usuario.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 
@@ -15,6 +25,22 @@ export class UsuarioService {
     };
     const createdUsuario = await this.prisma.usuario.create({ data });
     return { ...createdUsuario, senha_hash: undefined };
+  }
+
+  async findById(id: number) {
+    const usuario = await this.prisma.usuario.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        nome: true,
+        username: true,
+        email: true,
+        foto_perfil_url: true,
+      },
+    });
+
+    if (!usuario) throw new NotFoundException('Usuário não encontrado');
+    return usuario;
   }
 
   async findAll() {
@@ -33,26 +59,28 @@ export class UsuarioService {
 
   // Atualiza senha — valida a senha antiga antes
   async updateSenha(id: number, dto: UpdateSenhaDto) {
-  const usuario = await this.prisma.usuario.findUnique({ where: { id } });
+    const usuario = await this.prisma.usuario.findUnique({ where: { id } });
 
-  if (!usuario) {
-    throw new NotFoundException('Usuário não encontrado');
+    if (!usuario) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const senhaCorreta = await bcrypt.compare(
+      dto.senha_antiga,
+      usuario.senha_hash,
+    );
+    if (!senhaCorreta) {
+      throw new BadRequestException('Senha antiga incorreta');
+    }
+
+    const nova_senha_hash = await bcrypt.hash(dto.nova_senha, 10);
+    await this.prisma.usuario.update({
+      where: { id },
+      data: { senha_hash: nova_senha_hash },
+    });
+
+    return { message: 'Senha atualizada com sucesso' };
   }
-
-  const senhaCorreta = await bcrypt.compare(dto.senha_antiga, usuario.senha_hash);
-  if (!senhaCorreta) {
-    throw new BadRequestException('Senha antiga incorreta');
-  }
-
-  const nova_senha_hash = await bcrypt.hash(dto.nova_senha, 10);
-  await this.prisma.usuario.update({
-    where: { id },
-    data: { senha_hash: nova_senha_hash },
-  });
-
-  return { message: 'Senha atualizada com sucesso' };
-}
-  
 
   // Atualiza username — checa se já existe
   async updateUsername(id: number, dto: UpdateUsernameDto) {
