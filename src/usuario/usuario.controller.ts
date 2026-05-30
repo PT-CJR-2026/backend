49478e1945c61b349177b54e7219198ce397e289
Gabriel Severiano de Souza <gabriel.severianos@gmail.com>
2026-05-30 03:10:59 -0300

    fix: Remoção do nome da rota para não dar conflito com a integração de cadastro

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -23,7 +23,7 @@ export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @IsPublic()
-  @Post('cadastro')
+  @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
   }
